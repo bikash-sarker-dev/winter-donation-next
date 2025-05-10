fe45ab149d257ee66c20b5b60acd77f684232736
bikash sarker <bikash.webdeveloper5@gmail.com>
2025-05-10 23:36:10 +0600

    the right side add the searching option

--- src/components/detailsPage/RightSidebar.jsx
+++ src/components/detailsPage/RightSidebar.jsx
@@ -1,5 +1,32 @@
 const RightSidebar = () => {
-  return <div>RightSidebar</div>;
+  return (
+    <div className="bg-gray-100 h-screen p-4">
+      <h3>Search</h3>
+      <form action="">
+        <label className="input">
+          <svg
+            className="h-[1em] opacity-50"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+          >
+            <g
+              strokeLinejoin="round"
+              strokeLinecap="round"
+              strokeWidth="2.5"
+              fill="none"
+              stroke="currentColor"
+            >
+              <circle cx="11" cy="11" r="8"></circle>
+              <path d="m21 21-4.3-4.3"></path>
+            </g>
+          </svg>
+          <input type="search" className="grow" placeholder="Search" />
+          <kbd className="kbd kbd-sm">⌘</kbd>
+          <kbd className="kbd kbd-sm">K</kbd>
+        </label>
+      </form>
+    </div>
+  );
 };
 
 export default RightSidebar;
