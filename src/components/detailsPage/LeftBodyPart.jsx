5b8ab3c18030f4e0dd5924ebdd65c0c5d3f38ea3
bikash sarker <bikash.webdeveloper5@gmail.com>
2025-05-12 10:12:08 +0600

    the title chnage color or font size

--- src/components/detailsPage/LeftBodyPart.jsx
+++ src/components/detailsPage/LeftBodyPart.jsx
@@ -8,7 +8,7 @@ const LeftBodyPart = ({ campDetails }) => {
           <img src={image} alt="Shoes" />
         </figure>
         <div className="card-body">
-          <h2 className="card-title">{title}</h2>
+          <h2 className="card-title text-4xl font-bold">{title}</h2>
           <p>{description}</p>
           <div className="card-actions justify-end">
             <button className="btn btn-primary">Buy Now</button>
